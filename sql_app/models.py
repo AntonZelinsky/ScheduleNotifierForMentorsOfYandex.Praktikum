a67faa2da7712ed7fdf7bf81f41a0a9914695604
Александр Коростылев <punches@yandex.ru>
2021-10-13 00:17:26 +0300

    Add postgress and sqlalchemy moodels
    tables Users, Cohorts, Faculty

---
+++ sql_app/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "Users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True, index=True)
+    telegram_id = Column(Integer)
+    notion_us_id = Column(String)
+    is_activated = Column(Boolean, default=False)
+
+
+class Cohort(Base):
+    __tablename__ = "Cohorts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    number = Column(Integer)
+    faculty_id = Column(Integer, ForeignKey("Faculty.id"))
+    notion_db_id = Column(String)
+
+
+class Item(Base):
+    __tablename__ = "Faculty"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
