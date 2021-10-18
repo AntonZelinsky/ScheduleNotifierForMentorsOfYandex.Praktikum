db18c4b80dac2083b6ce6f3955858d842c0f7f4e
Vlad Nikitin <nikitinv91@gmail.com>
2021-10-18 14:24:55 +0300

    webhooks or polling can be used

--- bot.py
+++ bot.py
@@ -2,9 +2,12 @@ import datetime
 import logging
 import os
 
+from queue import Queue
+from threading import Thread
+
 from pytz import timezone
-from telegram import ParseMode
-from telegram.ext import CommandHandler
+from telegram import ParseMode, Bot
+from telegram.ext import CommandHandler, Dispatcher
 from telegram.ext import Updater, CallbackContext
 
 import notion
@@ -19,7 +22,7 @@ def start(update, context):
                  f'юзернеймом {update.effective_chat.username} и id {update.effective_chat.id}')
 
 
-def callback_morning_remainder(context: CallbackContext):
+def callback_morning_reminder(context: CallbackContext):
     user = notion.get_user_data()
 
     context.bot.send_message(chat_id=user.telegram_id,
@@ -28,7 +31,7 @@ def callback_morning_remainder(context: CallbackContext):
     logging.info(f'{user.name} c id {user.telegram_id} получил утреннее напоминание о дежурстве')
 
 
-def callback_evening_remainder(context: CallbackContext):
+def callback_evening_reminder(context: CallbackContext):
     user = notion.get_user_data()
 
     context.bot.send_message(chat_id=user.telegram_id,
@@ -39,23 +42,37 @@ def callback_evening_remainder(context: CallbackContext):
 
 def init():
     token = os.getenv('TELEGRAM_TOKEN')
+    webhook_url = os.getenv('DOMAIN_ADDRESS')
+    start_handler = CommandHandler('start', start)
+
+    """Если webhook_url не задан -> запускаем приложение через пуллинг."""
+    if webhook_url:
+        bot = Bot(token)
+        update_queue = Queue()
+
+        dispatcher = Dispatcher(bot, update_queue)
+        dispatcher.add_handler(start)
+
+        bot.set_webhook(webhook_url=webhook_url)
+        thread = Thread(target=dispatcher.start, name='dispatcher')
+        thread.start()
+        logging.info(f'Приложение работает через вебхук {webhook_url}')
+
+        return update_queue, dispatcher
+    
     updater = Updater(token, use_context=True)
 
-    morning_remainder_hour = int(os.getenv('MORNING_REMAINDER_HOUR', int))
-    time = datetime.time(hour=morning_remainder_hour, tzinfo=timezone("Europe/Warsaw"))
-    updater.job_queue.run_daily(callback_morning_remainder, time)
+    # вынести логику ремайндеров в методы, оформить в качестве хэндлеров?
+    morning_reminder_hour = int(os.getenv('MORNING_REMINDER_HOUR', int))
+    time = datetime.time(hour=morning_reminder_hour, tzinfo=timezone("Europe/Warsaw"))
+    updater.job_queue.run_daily(callback_morning_reminder, time)
 
-    evening_remainder_hour = int(os.getenv('EVENING_REMAINDER_HOUR', int))
-    time = datetime.time(hour=evening_remainder_hour, tzinfo=timezone("Europe/Warsaw"))
-    updater.job_queue.run_daily(callback_evening_remainder, time)
+    evening_reminder_hour = int(os.getenv('EVENING_REMINDER_HOUR', int))
+    time = datetime.time(hour=evening_reminder_hour, tzinfo=timezone("Europe/Warsaw"))
+    updater.job_queue.run_daily(callback_evening_reminder, time)
 
-    start_handler = CommandHandler('start', start)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(start_handler)
+    updater.start_polling()
 
-    logging.info("Приложение успешно запущено")
-    # registering a new webhook, start_polling replaced
-    # temp url provided by ngrok, move to Heroku?
-    updater.start_webhook(listen='0.0.0.0',
-                          port=80,
-                          webhook_url=f'https://2c62-188-242-211-2.ngrok.io/{token}')
+    logging.info('Приложение успешно запущено через пулинг')
