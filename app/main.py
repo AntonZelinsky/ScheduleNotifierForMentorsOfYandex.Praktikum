41566abea64670b3719ded4615b0af2e59398524
Anton Zelinsky <anton.zelinsky@gmail.com>
2021-10-24 17:59:44 +0200

    Fix bug and improve logic

--- app/main.py
+++ app/main.py
@@ -1,21 +1,8 @@
-import os
-import uvicorn
-
 from fastapi import FastAPI
-from pydantic import BaseModel
 from telegram import Update
 
 import bot
 from .api import router
-from core import models
-from core.database import engine
-
-# models.Base.metadata.create_all(bind=engine)
-
-# создаем модель для сообщения, получаемого с сервера ТГ
-class Message(BaseModel):
-    update_id: int
-    message: dict
 
 
 def create_app() -> FastAPI:
@@ -26,17 +13,10 @@ def create_app() -> FastAPI:
 
 app = create_app()
 
-# в таком виде dispatcher возвращается как NoneType
 dispatcher = bot.init()
 
+
 @app.post('/{token}/telegramWebhook')
-def webhook(update: dict):
-    """Эндпойнт, на который  приходят обновления из ТГ"""
-    update = Update.de_json(update, dispatcher)
+def webhook(data: dict):
+    update = Update.de_json(data, dispatcher.bot)
     dispatcher.process_update(update)
-    # return(update['message']['text'])
-
-
-# TODO вынести в отдельный файл
-def run_uvicorn():
-    uvicorn.run(app, host='0.0.0.0', port=80)
