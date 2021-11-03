25d0b2796dcfea71fc92534cf7d0deb31d51740e
Vlad Nikitin <nikitinv91@gmail.com>
2021-11-03 15:41:54 +0300

    basic conversation handler functionality added

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,29 +3,83 @@ import logging
 from queue import Queue
 from threading import Thread
 
-from pytz import timezone
-from telegram import ParseMode, Bot
-from telegram.ext import (CallbackContext,
-                          CommandHandler,
-                          Updater,
-                          Dispatcher,
-                          JobQueue)
-
 import notion
 from helpers import Objectify
-from ..core import config
+from pytz import timezone
+from telegram import Bot, ParseMode, ReplyKeyboardMarkup
+from telegram.ext import (CallbackContext, CommandHandler, ConversationHandler,
+                          Dispatcher, Filters, JobQueue, MessageHandler,
+                          Updater)
+
+from core.config import get_settings
+
+settings = get_settings()
+
+# вынести в отдельный файл с константами
+NAME, EMAIL = range(2)
+REPLY_KEYBOARD = [['Отправить письмо повторно', 'Изменить адрес почты']]
 
 
-settings = config.get_settings()
-
-
-def start(update, context):
+def start_test(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text=f"Привет, {update.effective_chat.full_name}, "
                                   "я буду присылать тебе уведомления о дежурстве в Я.П, "
                                   f"твой telegram id *{update.effective_chat.id}*.", parse_mode=ParseMode.MARKDOWN)
     logging.info(f'Добавился пользователь с именем {update.effective_chat.full_name}, '
                  f'юзернеймом {update.effective_chat.username} и id {update.effective_chat.id}')
+
+
+def start(update, context):
+    """ Начало диалога с новым пользователем """
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text=f"Привет! Для того, чтобы начать получать уведомления о дежурстве"
+                             " тебе необходимо зарегистрироваться.\n\nКак тебя зовут?")
+    logging.info(f'Добавился пользователь с именем {update.effective_chat.full_name}, '
+                 f'юзернеймом {update.effective_chat.username} и id {update.effective_chat.id}')
+    return NAME
+
+
+def request_email(update, context):
+    """ Запрашиваем имейл, к которому привязан аккаунт ноушена """
+    answer = update.message.text
+    if answer == REPLY_KEYBOARD[0][1]:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=f'Окей, давай изменим твою почту.\n👇')
+        logging.info(f'Пришел запрос на изменение почты')
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=f'Отлично!\n{answer}, для завершения регистрации'
+                                 ' пришли мне e-mail, к которому привязан твой аккаунт Notion.\n👇')
+        logging.info(f'У нас новый пользователь: {answer}')
+
+    return EMAIL
+
+
+def verify_email(update, context):
+    """
+    Отправляем письмо на почту для завершения регистрации.
+    Даем пользователю возможность изменить адрес или отправить подтверждение еще раз
+    """
+    email = update.message.text
+    if email == REPLY_KEYBOARD[0][0]:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='Отправили подтверждение еще раз.')
+        logging.info('Получен запрос на повторную отправку подтверждения')
+    elif email == REPLY_KEYBOARD[0][1]:
+        logging.info('Получен запрос на повторную отправку подтверждения')
+        request_email(update=update, context=context)
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=f'Супер, мы отправили письмо с подтверждением на почту {email}.'
+                                 '\nСледуй инструкциям в письме чтобы завершить процесс регистрации!',
+                                 reply_markup=ReplyKeyboardMarkup(REPLY_KEYBOARD, one_time_keyboard=True))
+        logging.info(f'Добавлен новый email: {email}')
+
+
+def stop(update, context):
+    """ Завершаем диалог """
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text='Увидимся!')
 
 
 def callback_morning_reminder(context: CallbackContext):
@@ -82,8 +136,18 @@ def init_pooling(token):
 
 def init():
     token = settings.telegram_token
-    webhook_url = f'{settings.domain_address}/{token}/telegramWebhook'
-    if webhook_url:
+    # webhook_url = f'{settings.domain_address}/{token}/telegramWebhook'
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            NAME: [MessageHandler(Filters.text, request_email)],
+            EMAIL: [MessageHandler(Filters.text, verify_email)]
+        },
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+
+    if settings.domain_address:
+        webhook_url = f'{settings.domain_address}/{token}/telegramWebhook'
         dispatcher = init_webhook(token, webhook_url)
     else:
         dispatcher = init_pooling(token)
@@ -94,7 +158,6 @@ def init():
     time = datetime.time(hour=settings.evening_reminder_hour, tzinfo=timezone("Europe/Warsaw"))
     dispatcher.job_queue.run_daily(callback_evening_reminder, time)
 
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(conv_handler)
 
     return dispatcher
--- core/config.py
+++ core/config.py
@@ -9,11 +9,13 @@ class BaseConfig(BaseSettings):
     Значение, предоставленное в этом классе,
     будет использовано в случае если оно не задано в .env файле
     """
+    debug: bool = True
     telegram_token: str
     notion_token: str
     notion_database_id: str
     morning_reminder_hour: int = 10
     evening_reminder_hour: int = 20
+    domain_address: str = None
 
     class Config:
         env_file = '.env'
@@ -22,22 +24,16 @@ class BaseConfig(BaseSettings):
 
 # TODO добавить необходимые перменные или методы для прода и тестов
 class DevelopmentConfig(BaseConfig):
-    debug: bool = True
     sqlalchemy_database_url: str = 'postgresql://root:root@localhost:35432/schedule_notifier'
 
 
 class TestConfig(BaseConfig):
-    debug: bool = True
     sqlalchemy_database_url: str = 'postgresql://root:root@localhost:35432/schedule_notifier'
-    domain_address: str
-    port: int = 80
 
 
 class ProductionConfig(BaseConfig):
     debug: bool = False
     sqlalchemy_database_url: str
-    domain_address: str
-    port: int = 80
 
 
 config = {
