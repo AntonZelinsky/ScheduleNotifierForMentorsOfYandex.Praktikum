1b4d50db0173e35cacf943e48841ccc9272643d7
Александр Коростылев <punches@yandex.ru>
2022-01-31 23:46:48 +0300

    make generator cycle

--- app/routers/cohorts.py
+++ app/routers/cohorts.py
@@ -4,6 +4,7 @@ from fastapi import Depends
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
 
+import notion
 from app.services import CohortService
 
 from .. import schemas
@@ -24,3 +25,12 @@ class CohortCBV:
     @router.post("/", response_model=schemas.Cohort)
     def create_cohort(self, cohort: schemas.CohortCreate):
         return self.service.create_cohort(cohort=cohort)
+
+    @router.get("/cycle")
+    def cycle(self):
+        cohorts = self.service.get_cohorts(skip=self.skip, limit=self.limit)
+        # for cohort in cohorts:
+        last_duties = notion.get_last_duties_by_cohort(cohorts[1])
+        cycle = notion.calculate_cycle_by_last_duties(last_duties['last_mentors_ids'])
+        added_duties = notion.add_duties_to_cohort(cohorts[1], cycle, last_duties['actual_date'])
+        return added_duties
--- notion.py
+++ notion.py
@@ -1,4 +1,5 @@
 import datetime
+from dateutil.parser import parse
 
 from notion_client import APIResponseError, Client
 from core import models
@@ -72,8 +73,10 @@ def get_users_raw_data(cohorts: list[Cohort]) -> list:
 
 
 def group_by_user_raw_data(raw_data: list) -> dict:
-    """Сгруппировать всех дежурных по юзерам,
-    т.к. один юзер может дежурить в нескольких когортах."""
+    """
+    Сгруппировать всех дежурных по юзерам,
+    т.к. один юзер может дежурить в нескольких когортах.
+    """
     users_group_data = {}
     for user_data in raw_data:
         if user_data.email in users_group_data:
@@ -90,3 +93,109 @@ def group_by_user_raw_data(raw_data: list) -> dict:
             }
             users_group_data.update(user_dist_data)
     return users_group_data
+
+
+def get_last_duties_by_cohort(cohort: Cohort, count_days: int = 7) -> dict:
+    """
+    Получить последовательность из `:count` крайних дежурств в обратном порядке.
+    Данные из прошлого не учитываем.
+    :param cohort: когорта,
+    :param count_days: количество дней для расчета,
+    :return:
+        {
+            'last_mentors_ids': tuple, крайние дежурства
+            'actual_date': date(%Y-%m-%d), крайняя заполненая дата (%Y-%m-%d)
+        }
+    """
+    client = create_client()
+    date_now = datetime.date.today().__str__()
+    response = client.databases.query(
+        **{
+            "database_id": cohort.notion_db_id,
+            "filter": {
+                "property": "Дата",
+                "date": {
+                    "on_or_after": date_now
+                }
+            },
+            "sorts": [{
+                "property": "Дата",
+                "timestamp": "created_time",
+                "direction": "descending"
+            }],
+            "page_size": count_days
+        }
+    )
+    response = Objectify(response)
+
+    last_duties = [duty.properties for duty in response.results]
+    last_duties = sorted(last_duties, key=lambda duty: duty.Дата.date.start, reverse=True)
+    actual_date = parse(last_duties[0].Дата.date.start).date()
+    last_mentors_ids = tuple(duty.Дежурный.people[0].id for duty in last_duties)
+    last_mentors = {
+        "last_mentors_ids": last_mentors_ids,
+        "actual_date": actual_date
+    }
+
+    return last_mentors
+
+
+def calculate_cycle_by_last_duties(last_duties: tuple) -> tuple:
+    """
+    Получить цикл из последовательности.
+    Подразумеваем, что цикл точно есть, минимум 1.
+    Что последовательность из элементов по одному.
+    Движение от 0-го элемента к последнему.
+    :return: tuple
+    """
+    cycle = set()
+    for mentor in last_duties:
+        if mentor not in cycle:
+            cycle.add(mentor)
+        else:
+            break
+    return tuple(cycle)
+
+
+def add_duties_to_cohort(cohort: Cohort, cycle: tuple, start_date: str, set_interval: int = 7):
+    client = create_client()
+    future_duties = [None] * set_interval
+    duty_date = start_date
+    for i, mentor in enumerate(future_duties):
+        duty_date += datetime.timedelta(days=1)
+        future_duties[i] = (dict(mentor=cycle[i % len(cycle)], date=duty_date))
+
+    return future_duties
+    # response = client.pages.create(
+    #     **{
+    #         "parent": {
+    #             "database_id": str(cohort.notion_db_id)
+    #         },
+    #         "properties": {
+    #             "Дата": {
+    #                 "date": {
+    #                     "start": "2022-02-13",
+    #                     "end": None
+    #                 }
+    #             },
+    #             "Name": {
+    #                 "title": [
+    #                     {
+    #                         "text": {
+    #                             "content": "Александр"
+    #                         }
+    #                     }
+    #                 ]
+    #             },
+    #             "Дежурный": {
+    #                 "people": [
+    #                     {
+    #                         "object": "user",
+    #                         "id": "134d1a34-7876-4cb5-8493-2875f6751a86"
+    #                     }
+    #                 ]
+    #             }
+    #         }
+    #     }
+    # )
+    # return future_duties
