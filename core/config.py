34831e5d0e0393da21b4f2864396894a7f336add
Vlad Nikitin <nikitinv91@gmail.com>
2022-03-07 20:34:14 +0300

    Initial setup of smtplib library for emails

--- core/config.py
+++ core/config.py
@@ -4,33 +4,13 @@ from fastapi_mail import ConnectionConfig
 from pydantic import BaseSettings
 
 
-class EmailConf(BaseSettings):
-    mail_username: str
-    mail_password: str
-    mail_from: str
-    mail_port: int = 587
-    mail_server: str
-    mail_from_name: str
-    mail_tls: bool = False
-    mail_ssl: bool = True
-    use_credentials: bool = True
-    validate_certs: bool = True
-    template_folder: str = './app/templates'
-
-    class Config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
-
-    def get_dict_upper_keys(self):
-        return dict((k.upper(), v) for k, v in self.__dict__.items())
-
-
 class BaseConfig(BaseSettings):
     """
     Базовый класс и валидатор для всех переменных окружения.
     Значение, предоставленное в этом классе,
     будет использовано в случае если оно не задано в .env файле
     """
+    # Bot
     environment: str = 'development'
     debug: bool = True
     telegram_token: str
@@ -39,14 +19,18 @@ class BaseConfig(BaseSettings):
     evening_reminder_hour: str = '20:15'
     domain_address: str = None
     port: int = 80
+
+    # Postgres
     sqlalchemy_database_url: str
     db_user: str
     db_password: str
     db_name: str
 
-    @staticmethod
-    def email_conf():
-        return ConnectionConfig(**EmailConf().get_dict_upper_keys())
+    # Mail
+    mail_username: str
+    mail_password: str
+    mail_port: int = 465
+    mail_server: str
 
     class Config:
         env_file = '.env'
